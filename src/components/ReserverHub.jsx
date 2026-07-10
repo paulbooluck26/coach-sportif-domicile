e704e8ebad0abba86b4764434887be5370d333d8
Paul Booluck <paulbooluck26@gmail.com>
2026-07-10 15:49:58 +0000

    File changes

--- src/components/ReserverHub.jsx
+++ src/components/ReserverHub.jsx
@@ -16,7 +16,7 @@ const OFFRES = [
     bouton: "Découvrir les programmes",
     lien: "/achat-programme",
     icon: Dumbbell,
-    image: "https://images.unsplash.com/photo-1534438327276-14e5300c3aad?w=800&q=80",
+    image: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=800&q=80",
   },
   {
     titre: "Parler de votre projet",
